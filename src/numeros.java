c6bd6997a6bf6e04c3b7cbfc3c65b97a6840a43b
DiAlPiRo <diegop.rosero@gmail.com>
2022-06-25 08:42:23 -0500

    primer cambio

---
+++ src/numeros.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class numeros {
+    public static void main(String args[]) {
+        for (int i = 1; i <= 100; i++)  {
+            System.out.println("numero: "+i);
+
+        }
+    }
+}
+
+
