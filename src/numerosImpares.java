ede931a1caec7cfbd72cf4664fd8fbc9b9df9ac1
DiAlPiRo <diegop.rosero@gmail.com>
2022-06-25 10:00:47 -0500

    son los pares xd

--- src/numerosImpares.java
+++ src/numerosImpares.java
@@ -1,6 +1,6 @@
 public class numerosImpares {
     public static void main(String args[]) {
-        for (int i = 1; i <= 3; i = i  + 1)  {
+        for (int i = 2; i <= 20; i = i  + 2)  {
             System.out.println("numero: "+i);
 
         }
