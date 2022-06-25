e07dd384f56ece4458f4e13fcbf2de7cbe0b356e
DiAlPiRo <diegop.rosero@gmail.com>
2022-06-25 09:18:37 -0500

    tercer cambio

--- src/numerosDeDosEnDos.java
+++ src/numerosDeDosEnDos.java
@@ -1,7 +1,13 @@
 public class numerosDeDosEnDos {
     public static void main(String args[]) {
-        for (int i = 2; i <= 20; i = i  + 2)  {
-            System.out.println("numero: "+i);
+        for (int i = 1; i <= 6; i++)  {
+            int e;
+            if (i%2==0) {
+                e=3;
+            }else {
+                e=1;
+            }
+            System.out.println("numero: "+e);
 
         }
     }
---
+++ src/numerosImpares.java
@@ -0,0 +1,9 @@
+public class numerosImpares {
+    public static void main(String args[]) {
+        for (int i = 1; i <= 3; i = i  + 1)  {
+            System.out.println("numero: "+i);
+
+        }
+    }
+}
+
